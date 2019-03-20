7c591db111c56602417c39d8f5bfe8764a687343
Sushant <41162773+sushi200@users.noreply.github.com>
2019-03-21 00:02:43 +0530

    Comments added

--- FuzzBuzz.js
+++ FuzzBuzz.js
@@ -1,16 +1,19 @@
+/*Feel free review this code */
+
 var i;
-for (i=0;i<=100;i++)
+for (i=0;i<=100;i++) /*For loop for printing the n numbers. In this case 1 to 100*/
 {
-    if (i%3==0 && i%5==0){
+    if (i%3==0 && i%5==0){                      /* if statement used inside for loop to check wether a number is divisible by 
+                                                   by both 3 and 5 */
         console.log("FuzzBuzz");
     }
-    else if (i%3==0){
+    else if (i%3==0){                           /* checks if a number is only divisible by 3 and print "Fuzz"*/
         console.log("Fuzz");
     }
-    else if(i%5==0){
+    else if(i%5==0){                             /*checks if a number by only 5 and print "Buzz"*/
         console.log("Buzz");
     }
     else {
-        console.log(i)
+        console.log(i)                           /*it prints the usual number if the above cases are not satisfied*/
     }
-}console.log(i)
+}
